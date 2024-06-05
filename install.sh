f9b7f09f901c8af3dcba3786f6a67de7061848e0
gouravkohad <gouravsourav77@gmail.com>
2024-06-05 20:55:35 +0530

    update

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Welcome in Besty.js"
   
   pkg install git
   cd $1
-  git clone https://github.com/gouravkohad/Besty.git 
+  git clone https://github.com/gouravkohad/Besty/Besty.git 
   
   echo "Setup completed"
   
