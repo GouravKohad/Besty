4a0f285f7c8d44573c2d2751b1ad910c8b59731e
Gourav <gouravsourav77@gmail.com>
2024-06-05 20:43:42 +0530

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+
+echo "Welcome in Besty.js"
+
+  
+  pkg install git
+  cd $1
+  git clone https://github.com/gouravkohad/Besty.git 
+  
+  echo "Setup completed"
+  
---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+
+document.querySelector('#get').addEventListener('click',()=> {
+   document.querySelector('.main2').scrollIntoView({behavior:'smooth'})
+})
+
+document.querySelector('.install').addEventListener('click',()=> {
+   document.querySelector('.main3').scrollIntoView({behavior:'smooth'})
+})
+
+document.querySelector('.docs').addEventListener('click',()=> {
+   document.querySelector('.main4').scrollIntoView({behavior:'smooth'})
+})
+
